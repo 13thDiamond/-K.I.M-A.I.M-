c418b1c040cdd18f09802e4fce1e1bb4b9069c9c
Wilhelm Wagner <128723350+13thDiamond@users.noreply.github.com>
2023-12-20 16:38:01 +0100

    kim_main.py
    Create and added the code for "kim_main.py"

---
+++ kim_main.py
@@ -0,0 +1,41 @@
+import sys
+import time
+from pynput import keyboard
+from kim import Kim
+
+
+kim = Kim()
+
+def on_press(key):
+    try:
+        char = None
+        if hasattr(key, 'char'):
+            char = key.char
+        else:
+            char = key.name
+        
+        if char == 's':
+            kim.start()
+            print("\n[KIM] Is running {°o°}")
+        
+        elif char == 'p':
+            kim.pause()
+            print("\n[KIM] Is resting {≠,≠}")
+        
+        elif char == 'r':
+            kim.resume()
+            print("\n[KIM] Is running agian {°o°}")
+        
+        elif key == keyboard.Key.esc:
+            print("\n[KIM] Says Goodbye see ya {^_^}")
+            kim.clean_exit()
+            sys.exit()
+            
+    except AttributeError:
+        print('[KIM] Is confused @,@...')
+        pass
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
+
+print("Thread Status:", kim.is_alive())
